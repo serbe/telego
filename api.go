e4cb421b55eabd52d8d5f1a1f0884053f06ea7f5
Serb <mkoctuk@gmail.com>
2017-01-24 15:15:54 +0300

    api struct

---
+++ api.go
@@ -0,0 +1,12 @@
+package telego
+
+import "encoding/json"
+
+// Response - response from the Telegram API
+type Response struct {
+	Ok          bool                `json:"ok"`
+	Result      json.RawMessage     `json:"result"`
+	ErrorCode   int                 `json:"error_code"`
+	Description string              `json:"description"`
+	Parameters  *ResponseParameters `json:"parameters"`
+}
