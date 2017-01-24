739b0b696f6d233aa0750b1cc2ae3cbe67884818
Serb <mkoctuk@gmail.com>
2017-01-24 15:18:09 +0300

    create bot

---
+++ telego.go
@@ -0,0 +1,36 @@
+package telego
+
+import "net/http"
+
+const urlAPI = "https://api.telegram.org/bot%s/%s"
+
+var (
+	botDebugLog bool
+	botErrorLog bool
+)
+
+// Telebot - telego bot main struct
+type Telebot struct {
+	Token  string
+	Client *http.Client
+	Self   User
+}
+
+func newBot(token string, debugLog bool, errorLog bool) (*Telebot, error) {
+	botDebugLog = debugLog
+	botErrorLog = errorLog
+
+	bot := new(Telebot)
+	bot.Token = token
+	bot.Client = new(http.Client)
+
+	user, err := bot.getMe()
+	if err != nil {
+		errLog("bot.getMe", err)
+		return &Telebot{}, err
+	}
+
+	bot.Self = user
+
+	return bot, nil
+}
